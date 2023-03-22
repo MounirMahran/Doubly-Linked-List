15a62adf7ad8e7ea21318a3311c78596d6f543da
MounirMahran <mounirmahran21@gmail.com>
2023-03-22 13:30:02 +0200

    Added Insert At Begining Feature

--- Doubly-Linked-List/Doubly-Linked-List/Doubly-Linked-List.cpp
+++ Doubly-Linked-List/Doubly-Linked-List/Doubly-Linked-List.cpp
@@ -8,6 +8,60 @@ public:
     Node* Previous;
 };
 
+void InsertAtBegining(Node*& Head, int Value) {
+    Node* NewNode = new Node();
+
+    NewNode->Value = Value;
+    NewNode->Previous = NULL;
+
+   
+    if (Head != NULL) {
+        NewNode->Next = Head;
+        Head->Previous = NewNode;
+    }
+
+    Head = NewNode;
+}
+
+void PrintNodeDetails(Node* Head)
+{
+
+    if (Head->Previous != NULL)
+        cout << Head->Previous->Value;
+    else
+        cout << "NULL";
+
+    cout << " <--> " << Head->Value << " <--> ";
+
+    if (Head->Next != NULL)
+        cout << Head->Next->Value << "\n";
+    else
+        cout << "NULL";
+
+}
+
+void PrintListDetails(Node* Head)
+
+{
+    cout << "\n\n";
+    while (Head != NULL) {
+        PrintNodeDetails(Head);
+        Head = Head->Next;
+    }
+}
+
+void PrintList(Node* Head)
+
+{
+    cout << "NULL <--> ";
+    while (Head != NULL) {
+        cout << Head->Value << " <--> ";
+        Head = Head->Next;
+    }
+    cout << "NULL";
+
+}
+
 int main()
 {
     Node* Head;
@@ -33,12 +87,13 @@ int main()
     Node3->Next = NULL;
     Node3->Previous = Node2;
 
+   
+
     //Printing list
     Head = Node1;
 
-    while (Head != NULL)
-    {
-        cout << Head->Value << endl;
-        Head = Head->Next;
-    }
+    InsertAtBegining(Head, 12);
+
+    PrintList(Head);
+    PrintListDetails(Head);
 }
